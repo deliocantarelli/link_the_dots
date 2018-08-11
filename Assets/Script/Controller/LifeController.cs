ed1d29c68456ff56f297befd51ab249c8929e583
Délio Cantarelli <delio@manifestohut.com.br>
2018-08-11 17:55:38 -0300

    added timer

--- Assets/Script/Controller/LifeController.cs
+++ Assets/Script/Controller/LifeController.cs
@@ -6,6 +6,8 @@ public class LifeController : MonoBehaviour
 {
 	public static LifeController Instance { get; private set; }
 	public int startLifes = 3;
+	public int maximumTime = 20;
+	private float currentTime;
 	private int currentLifes;
 	private int currentScore;
     // Use this for initialization
@@ -22,12 +24,14 @@ public class LifeController : MonoBehaviour
 	private void Init()
 	{
 		currentLifes = startLifes;
+		currentTime = maximumTime;
 	}
 
 	// Update is called once per frame
 	void Update()
     {
-
+		currentTime -= Time.deltaTime;
+		CheckTimeOut();
     }
 
 	public void OnShapeFinished(GameShape shape, bool isCorrect) {
@@ -37,19 +41,27 @@ public class LifeController : MonoBehaviour
 			Award(shape);
 		}
 	}
+	public int GetCurrentTime() {
+		return Mathf.CeilToInt(currentTime);
+	}
+
 	private void Award(GameShape shape) {
 		currentScore++;
-		Debug.Log("score: " + currentScore);
 	}
 	private void Penality(GameShape shape) {
 		currentLifes--;
-		Debug.Log("lifes: " + currentLifes);
 		CheckDefeatCondition();
 	}
 
 	private void CheckDefeatCondition() {
 		if(currentLifes <= 0) {
 			OnDefeat();	
+		}
+	}
+	private void CheckTimeOut() {
+		Debug.Log(GetCurrentTime());
+		if(currentTime <= 0) {
+			OnDefeat();
 		}
 	}
 
